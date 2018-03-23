6cc589914c2a68709c994c46e3ce2031c51139d5
Julien CHATEAU <julien.chateau@etu.univ-nantes.fr>
2018-03-23 08:38:28 +0100

    efzafze

--- script/codePostalComplete.js
+++ script/codePostalComplete.js
@@ -1,49 +1,24 @@
 $(document).ready(function() {
 
     $("#commune").autocomplete({
-      // var tmp = this["bindings"][0]["attributes"]["id"]["value"];
       source: function(request, response) {
         $.ajax({
           url : "http://infoweb-ens/~jacquin-c/codePostal/commune.php",
           method : "GET",
           data : {
-            commune : request.term
+            commune : request.term,
+            maxRows : 15
           },
           success : function(data) {
             var parse = data.map(function(ligne) {
               return {
-                label : ligne.CodePostal+"  "+ligne.Ville,
-                value : (tmp == "commune") ? ligne.Ville : ligne.CodePostal,
+                label : ligne.Ville,
+                value : ligne.Ville,
               };
             });
             response(parse);
           }
         });
-      },
-      select: function(event, ui) {
-        $("#codePostal").val(ui.item["oth"]);
-        $("#commune").val(ui.item["oth"]);
       }
   });
 });
-
-function afficherCodePostal(communeStr) {
-  var ajax = $.ajax({
-    url : "codePostal.php",
-    method : "GET",
-    data : {
-      commune : communeStr
-    }
-  });
-  ajax.done(function(data) {
-    $(".liste ul li").remove();
-    if ($(data).length != 0) {
-      $(data).each(function(index, val) {
-        $(".liste ul").append("<li>"+val.CP+"; "+val.CODEPAYS+"</li>");
-      })
-    }
-  });
-  ajax.fail(function() {
-    console.log("Erreur retour ajax");
-  });
-}
