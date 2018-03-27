f548497b1477ab288b0e16ce73e4dda6e178089f
kovinns <kovinns@hotmail.fr>
2018-03-27 08:23:43 +0200

    h

--- script/codePostalComplete.js
+++ script/codePostalComplete.js
@@ -87,8 +87,8 @@ function envoieRequette(ville, nombre){
             },
             success: function(dataInfo){
               console.log(dataInfo);
-              $('ul').append('<li><img src="https://farm' + img.farm + '.staticflickr.com/' + img.server + '/' + img.id + '_' + img.secret + '.jpg"/></li>');
-              $('table').append('<tr><td><img src="https://farm' + img.farm + '.staticflickr.com/' + img.server + '/' + img.id + '_' + img.secret + '.jpg"/></td></tr>');
+              $('ul').append('<li><img src="https://farm' + img.farm + '.staticflickr.com/' + img.server + '/' + img.id + '_' + img.secret + '.jpg"/><div>' + dataInfo.photo.title._content + '</div></li>');
+              $('table').append('<tr><td><img src="https://farm' + img.farm + '.staticflickr.com/' + img.server + '/' + img.id + '_' + img.secret + '.jpg"/></td><td>' + dataInfo.photo.title._content + '</td></tr>');
             }
           });
         });
