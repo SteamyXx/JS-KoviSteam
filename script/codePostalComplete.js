7569c5c117141c3b89fdee2146a727ac343a0f79
kovinns <kovinns@hotmail.fr>
2018-03-27 12:20:42 +0200

    jkshdjkqsdhqj

--- script/codePostalComplete.js
+++ script/codePostalComplete.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
       });
     },
     select : function(event, ui) {
-      if($("#nombreParPage").val() != ""){
+      if($("#nombreParPage").val() != "" && $('#calendrier').val() != ""){
         envoieRequette(ui.item.value, $("#nombreParPage").val());
       }
       champCommune = ui.item.value;
@@ -58,7 +58,7 @@ $(document).ready(function() {
   });
 
   $("#nombreParPage").change(function(){
-    if($("#commune").val() != "" && $("#nombreParPage").val() != ""){
+    if($("#commune").val() != "" && $("#nombreParPage").val() != "" && $('#calendrier').val() != ""){
       envoieRequette($("#commune").val(), $("#nombreParPage").val());
 
     }
@@ -66,14 +66,14 @@ $(document).ready(function() {
 
   $('#boutonSubmit').click(function(event){
     event.preventDefault();
-    if($("#commune").val() != "" && $("#nombreParPage").val() != ""){
+    if($("#commune").val() != "" && $("#nombreParPage").val() != "" && $('#calendrier').val() != ""){
       envoieRequette($("#commune").val(), $("#nombreParPage").val());
     }
   });
 
 });
 
-function envoieRequette(ville, nombre){
+function envoieRequette(ville, nombre, date){
   $.ajax({
     url: "https://api.flickr.com/services/rest/",
     method: "GET",
@@ -83,7 +83,8 @@ function envoieRequette(ville, nombre){
       tags: ville,
       format: "json",
       nojsoncallback: "1",
-      per_page: nombre
+      per_page: nombre,
+      min_taken_date: date
     },
     success: function(dataImg){
       $('.ligneListe').remove();
@@ -123,6 +124,8 @@ function envoieRequette(ville, nombre){
       }
     }
   });
+
+  $("table").dataTable();
 
 
 function remplirModal(id, secret) {
